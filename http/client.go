68326b097f1de2b699a49220e28a6232415256e2
John Boiles <johnaboiles@gmail.com>
2016-02-03 17:01:11 -0800

    Support for adding headers (to support Google Authorization Bearer tokens)

--- http/client.go
+++ http/client.go
@@ -7,8 +7,16 @@ import (
 
 // a client for making HTTP requests
 type Client struct {
-	native *http.Client
-	server *Server
+	native         *http.Client
+	server         *Server
+	requestHeaders map[string]string
+}
+
+func (c *Client) SetHeader(key string, value string) {
+	if c.requestHeaders == nil {
+		c.requestHeaders = map[string]string{}
+	}
+	c.requestHeaders[key] = value
 }
 
 // downcasts to the native HTTP interface
@@ -23,6 +31,9 @@ func (c *Client) Server() *Server {
 
 // executes an HTTP request
 func (c *Client) Do(req *Request) (*Response, error) {
+	for key, value := range c.requestHeaders {
+		req.Header.Add(key, value)
+	}
 	if resp, err := c.Native().Do((*http.Request)(req)); err != nil {
 		return nil, utils.NewError(c.Do, "unable to execute HTTP request", c, err)
 	} else {
